83f9f436414ae7404bd0f129ff778fe5d9d7f2da
sjMun09 <sjMun09>
2023-04-02 23:33:30 +0900

    세팅 끝

---
+++ src/main/java/HelloServlet.java
@@ -0,0 +1,19 @@
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("UTF-8");
+        resp.setCharacterEncoding("UTF-8");
+        resp.setContentType("text/html; charset-utf-8");
+
+        resp.getWriter().append("안녕하세요. 이제 시작하겠습니다.");
+    }
+}
